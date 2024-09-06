c18aec67d283496020b082db7649d57522b9d425
Ashta <33148617+ashtav@users.noreply.github.com>
2024-09-06 16:28:41 +0800

    update

---
+++ lib/toast_examples/toast_view.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class ToastView extends StatelessWidget {
+  const ToastView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // kalau ingin lebih simple dan cepat dalam penulisan kode, buatkan 1 method
+
+    void showToast(String message) {
+      Fluttertoast.showToast(
+          msg: message,
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black54,
+          textColor: Colors.white,
+          fontSize: 16.0);
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Toast Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+                onPressed: () {
+                  Fluttertoast.showToast(
+                      msg: "This is Center Short Toast",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0);
+                },
+                child: const Text('Show Toast')),
+            const SizedBox(height: 15),
+            TextButton(
+                onPressed: () {
+                  showToast('Hello nama saja John Doe!');
+                },
+                child: const Text('Show My Name'))
+          ],
+        ),
+      ),
+    );
+  }
+}
